9cff0d8182b09575ccf45814105c65b302741259
Apoh Eldrige <peldrige8@gmail.com>
2021-03-19 12:49:56 +0100

    finish search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, Col } from 'react-bootstrap';
 
 const SearchForm = ({ params, onParamChange }) => {
   return (
     <Form>
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control
@@ -12,6 +12,25 @@ const SearchForm = ({ params, onParamChange }) => {
             value={params.description}
             name="description"
             type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            onChange={onParamChange}
+            value={params.location}
+            name="description"
+            type="text"
+          />
+        </Form.Group>
+        <Form.Group as={Col} className="ml-2">
+          <Form.Check
+            onChange={onParamChange}
+            value={params.full_time}
+            name="full_time"
+            id="full time"
+            label="Only full time"
+            className="mb-2"
           />
         </Form.Group>
       </Form.Row>
