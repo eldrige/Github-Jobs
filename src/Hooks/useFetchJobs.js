63391a70094a42b2ac7aea65d82b4b26e4ee2a71
Apoh Eldrige <peldrige8@gmail.com>
2021-03-22 12:08:23 +0100

    add cors header

--- src/Hooks/useFetchJobs.js
+++ src/Hooks/useFetchJobs.js
@@ -2,7 +2,7 @@ import { useReducer, useEffect } from 'react';
 import { REQUEST, ERROR, GET_DATA, UPDATE_HAS_NEXT_PAGE } from '../constants';
 import axios from 'axios';
 
-const BASE_URL = `https://jobs.github.com/positions.json`;
+const BASE_URL = `https:api.allorigins.win/raw?url=https://jobs.github.com/positions.json`;
 
 // const proxies = [
 //   `https://cors-anywhere.herokuapp.com/`,
@@ -48,6 +48,7 @@ const useFetchJobs = (params, page) => {
     axios
       .get(BASE_URL, {
         params: { markdown: true, page: page, ...params },
+        headers: { 'Access-Control-Allow-Origin': '*' },
       })
       .then((res) => {
         dispatch({
@@ -66,6 +67,7 @@ const useFetchJobs = (params, page) => {
     axios
       .get(BASE_URL, {
         params: { markdown: true, page: page + 1, ...params },
+        headers: { 'Access-Control-Allow-Origin': '*' },
       })
       .then((res) => {
         dispatch({
